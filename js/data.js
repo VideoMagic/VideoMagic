013bd62ab9e2c08eb5a78c998bb070b8afe9aa92
Jaeyoon Song <jyo3on@gmail.com>
2018-12-11 01:27:17 +0900

    user study template

---
+++ js/data.js
@@ -0,0 +1,205 @@
+let data = [  
+    {  
+       "start":"0:12",
+       "end":"0:32",
+       "speaker":"Jaeyoon",
+       "memo":"we can start today we're going to talk about which buffet we're going to go in Hilton Hotel at Gangnam station. It will be on December 28th from 6 p.m. to 9 p.m..",
+       "type":"Topic Conversion"
+    },
+    {  
+       "start":"0:38",
+       "end":"0:44",
+       "speaker":"Geunyoung",
+       "memo":"Menu",
+       "type":"Topic Conversion"
+    },
+    {  
+       "start":"0:38",
+       "end":"0:44",
+       "speaker":"Geunyoung",
+       "memo":"I like meat better than seafood so I want to go to Santa Buffet.",
+       "type":"Idea"
+    },
+    {  
+       "start":"0:46",
+       "end":"0:50",
+       "speaker":"Dahee",
+       "memo":"likes meat too",
+       "type":"Idea"
+    },
+    {  
+       "start":"0:54",
+       "end":"0:58",
+       "speaker":"Seyong",
+       "memo":"but I like seafood.",
+       "type":"Idea"
+    },
+    {  
+       "start":"1:8",
+       "end":"1:25",
+       "speaker":"Jaeyoon",
+       "memo":"what about Jaeyoon? Well, I actually like to eat but all I think I like seafood better but both are okay.",
+       "type":"Idea"
+    },
+    {  
+       "start":"1:25",
+       "end":"1:38",
+       "speaker":"Jaeyoon",
+       "memo":"Maybe we should think about other things too => Price",
+       "type":"Topic Conversion"
+    },
+    {  
+       "start":"1:48",
+       "end":"1:54",
+       "speaker":"Geunyoung",
+       "memo":"Santa buffet expensive.",
+       "type":"Idea"
+    },
+    {  
+       "start":"2:13",
+       "end":"2:22",
+       "speaker":"Jaeyoon",
+       "memo":"bunker Buffet is more expensive but there are some discounts so let's look at the discounts.",
+       "type":"Idea"
+    },
+    {  
+       "start":"2:22",
+       "end":"2:26",
+       "speaker":"Jaeyoon",
+       "memo":"anyone less than 18 years old? => No",
+       "type":"Idea"
+    },
+    {  
+       "start":"2:37",
+       "end":"2:38",
+       "speaker":"Geunyoung",
+       "memo":"how about birthday?",
+       "type":"Idea"
+    },
+    {  
+       "start":"2:40",
+       "end":"2:51",
+       "speaker":"Dahee",
+       "memo":"I guess Sarah's birthday is in December.",
+       "type":"Idea"
+    },
+    {  
+       "start":"2:58",
+       "end":"3:1",
+       "speaker":"Seyong",
+       "memo":"I use IBK Bank.",
+       "type":"Idea"
+    },
+    {  
+       "start":"3:2",
+       "end":"3:4",
+       "speaker":"Dahee",
+       "memo":"cacao bank",
+       "type":"Idea"
+    },
+    {  
+       "start":"3:36",
+       "end":"3:38",
+       "speaker":"Dahee",
+       "memo":"calculation: almost same",
+       "type":"Custom"
+    },
+    {  
+       "start":"3:42",
+       "end":"3:52",
+       "speaker":"Geunyoung",
+       "memo":"And how about another things? How about dessert? Does anybody like like dessert?",
+       "type":"Topic Conversion"
+    },
+    {  
+       "start":"3:56",
+       "end":"3:59",
+       "speaker":"Seyong",
+       "memo":"I love macron.",
+       "type":"Idea"
+    },
+    {  
+       "start":"4:22",
+       "end":"4:35",
+       "speaker":"Jaeyoon",
+       "memo":"I'm also a big fan of macaron so I also prefer Seafood Buffet I think.",
+       "type":"Idea"
+    },
+    {  
+       "start":"4:35",
+       "end":"4:39",
+       "speaker":"Dahee",
+       "memo":"I like macaron too.",
+       "type":"Idea"
+    },
+    {  
+       "start":"4:53",
+       "end":"4:58",
+       "speaker":"Seyong",
+       "memo":"size and floor",
+       "type":"Topic Conversion"
+    },
+    {  
+       "start":"4:53",
+       "end":"5:2",
+       "speaker":"Jaeyoon",
+       "memo":"6 people including David and Sarah.",
+       "type":"Idea"
+    },
+    {  
+       "start":"5:10",
+       "end":"5:21",
+       "speaker":"Seyong",
+       "memo":"Santa > Seafood",
+       "type":"Idea"
+    },
+    {  
+       "start":"5:27",
+       "end":"5:59",
+       "speaker":"Geunyoung",
+       "memo":"and also we have to consider about the mood.",
+       "type":"Topic Conversion"
+    },
+    {  
+       "start":"5:51",
+       "end":"5:55",
+       "speaker":"Dahee",
+       "memo":"romantic",
+       "type":"Idea"
+    },
+    {  
+       "start":"6:13",
+       "end":"6:25",
+       "speaker":"Seyong",
+       "memo":"I don't like the underground.",
+       "type":"Idea"
+    },
+    {  
+       "start":"6:30",
+       "end":"6:33",
+       "speaker":"Jaeyoon",
+       "memo":"window => view",
+       "type":"Idea"
+    },
+    {  
+       "start":"6:38",
+       "end":"6:46",
+       "speaker":"Dahee",
+       "memo":"review",
+       "type":"Topic Conversion"
+    },
+    {  
+       "start":"6:55",
+       "end":"7:6",
+       "speaker":"Jaeyoon",
+       "memo":"Santa Buffet is 3 * and seafood is 4 *.",
+       "type":"Idea"
+    },
+    {  
+       "start":"7:13",
+       "end":"7:21",
+       "speaker":"Geunyoung",
+       "memo":"Seafood > Santa",
+       "type":"Decision"
+    }
+ ];
---
+++ js/main.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function(e) {
+    // Your code to run since DOM is loaded and ready
+
+    let container = document.querySelector(".boxes");
+    let types = ["Topic Conversion", "Idea", "Decision", "Custom"];
+    let counter = [0, 0, 0, 0];
+    let total = 0;
+
+    let result = "";
+
+    data.forEach(function(ele, i) {
+
+        let typeNumber = types.indexOf(ele.type);
+        counter[typeNumber]++;
+        total++;
+
+        result += '<div class="box b' + typeNumber + ' margin' + i + '"><b>' + ele.type + '</b> <span class="speaker ' + ele.speaker + '">(' + ele.speaker + ')</span><br>' + 
+        '<span class="time">' + ele.start + '- 0:32</span><br>' + 
+        '<div class="memo">' + ele.memo + '</div></div>';
+    })
+
+    container.innerHTML += ("<div><span class='color0'>" + counter[0] + " Topic Conversions</span> / <span class='color1'>" + counter[1] + " Idea & Suggestions </span> / <span class='color2'>" + counter[2] + " Decision </span><br>TOTAL: " + total + " items</div><br>");
+    container.innerHTML += result;
+});
